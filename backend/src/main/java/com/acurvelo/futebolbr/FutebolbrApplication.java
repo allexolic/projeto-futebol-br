33557bea69a55e35e60285d1b0981342365786c2
Alexandre Oliveira Curvelo <allexolic@gmail.com>
2021-05-10 22:17:49 -0300

    first commit

---
+++ backend/src/main/java/com/acurvelo/futebolbr/FutebolbrApplication.java
@@ -0,0 +1,13 @@
+package com.acurvelo.futebolbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutebolbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutebolbrApplication.class, args);
+	}
+
+}
